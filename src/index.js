77f64d34423abe22203dfcbf48cacad502d012ae
Kuttivel-Dharmaraj <kuttiveldharmaraj@gmail.com>
2024-12-19 12:54:21 +0000

    Normalize line endings

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import FacebookEmojiCounter from './Facebookemoji';
+import ToggleMode from './ToggleModeComponent';
+
+
+ReactDOM.render(
+    <React.Fragment>
+        <FacebookEmojiCounter type="Like" />
+        <FacebookEmojiCounter type="Love" />
+        <FacebookEmojiCounter type="happy" />
+        <ToggleMode />
+    </React.Fragment>,
+    document.getElementById('root')
+);
+
+
+
